ddc541286930f2b46a8d29f8379d9988c88606d3
tanay-nagde <tanaynagde@gmail.com>
2024-07-18 16:49:47 +0530

    about page added

---
+++ src/components/Aboutpage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Aboutpage() {
+  return (
+    <>
+    <div className='py-10 px-4 w-full h-full '>
+    <div className=" bg-blue-950/10 rounded-2xl w-full overflow-hidden">
+    <div className='w-11/12 mx-auto'>
+
+    <div className='w-full'>
+    <h1 className="md:text-6xl text-4xl text-blue-300 font-bold text-center racing-sans-one-regular">
+          ABOUT US
+        </h1>
+        <hr className="bg-blue-900  md:w-1/6 w-1/2 mx-auto border-none my-2 h-1" />
+        <p className='text-left'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium, at voluptatem? Ullam similique sequi beatae vitae quas, atque nobis, obcaecati incidunt laboriosam facere maxime temporibus hic rem molestias odio possimus, laudantium expedita. Id libero eum delectus atque quam nihil consequuntur aliquam officia, itaque molestias recusandae eaque quo iure amet voluptatum. Natus quia velit, incidunt nulla ipsum ad adipisci quod! Consequatur nisi nihil reprehenderit reiciendis maiores repudiandae? Officiis autem beatae eius optio quaerat labore amet atque eaque saepe doloribus quos, inventore qui aliquam eum illo nobis, sed ab. Totam suscipit est dignissimos commodi culpa, dicta magni ipsa asperiores delectus nulla veritatis
+        </p>
+    </div>
+
+    </div>
+    </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Aboutpage
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Aboutpage from './components/Aboutpage.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/Home.jsx'
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
     children: [
       {path: "/", element: <Home/>},
+      {path: "/aboutus", element: <Aboutpage/>}
     ]
   },
 ]);
