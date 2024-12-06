9d47389133b518fc319b78acba82c947ab4b5e5e
tanay-nagde <tanaynagde@gmail.com>
2024-12-06 12:09:08 +0530

    hover fixed

--- src/component/Glowcard.jsx
+++ src/component/Glowcard.jsx
@@ -5,7 +5,7 @@ function Glowcard(props) {
   return (
     <div
       className="h-[28rem] w-96 roboto-regular flex flex-col rounded-lg my-2 p-3 overflow-hidden 
-        items-center  bg-blue-800/10 hover:scale-105 ease-in-out border-2 gap-10 border-blue-600 duration-300"
+        items-center  bg-blue-800/10 hover:scale-[1.02] ease-in-out border-2 gap-10 border-blue-600 duration-300"
     >
       <div className="flex justify-center items-center h-1/2  w-11/12 ">
         <img className=" w-full" src={props.img} alt={props.title} />
