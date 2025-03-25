4869f19acc4952130978fae631f96b4e822a3a62
tanay-nagde <tanaynagde@gmail.com>
2025-03-26 02:02:08 +0530

    prod fix 2

--- src/component/Contactform.jsx
+++ src/component/Contactform.jsx
@@ -15,7 +15,7 @@ export default function ContactForm() {
     const formData = new FormData(form.current);
   
     try {
-      const response = await fetch("/api/sendEmail", {
+      const response = await fetch("https://gsracersweb.vercel.app/api/sendEmail", {
         method: "POST",
         body: JSON.stringify({
           name: formData.get("name"),
